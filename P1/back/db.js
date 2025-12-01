386d5338f0c0cbbafc3e587048a45de9678ba712
Vinicius <vini.familalima@gmail.com>
2025-12-01 14:07:13 -0300

    Conexão BD

--- P1/back/db.js
+++ P1/back/db.js
@@ -1,8 +1,10 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 
-export const db = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "bcttscrgg",
-        database: "crud"
+export const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
