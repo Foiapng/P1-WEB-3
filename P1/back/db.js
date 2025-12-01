9bb3dc09123463637bdc93d18ea4ab95db025778
Vinicius <vini.familalima@gmail.com>
2025-12-01 14:17:40 -0300

    Conexão BD

--- P1/back/db.js
+++ P1/back/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2";
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST,   // apenas o host
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,   // importante: 20863
   waitForConnections: true,
   connectionLimit: 10,
 });
